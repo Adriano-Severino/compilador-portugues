f79992828d679d286178974e5e11ef1b6cb6c62f
Adriano-Severino <adriano.x1@hotmail.com>
2025-06-07 20:28:02 -0300

    Adiciona script de compilação rápida para facilitar a geração de executáveis

---
+++ compile_fast.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PROGRAMA=$1
+
+if [ -z "$PROGRAMA" ]; then
+    echo "Uso: $0 <arquivo_sem_extensão>"
+    exit 1
+fi
+
+echo "Compilando $PROGRAMA.pr..."
+
+# Usar o compilador já construído
+./target/release/meu_compilador $PROGRAMA.pr
+
+# Gerar executável diretamente
+clang -O3 $PROGRAMA.ll -o $PROGRAMA
+
+echo "Pronto! Execute com: ./$PROGRAMA"
